9c4bbdc902f4cee6d7c0339e5e1af0c2da5666f4
kurt1s <knbsk8@gmail.com>
2019-02-13 16:40:48 -0500

    First commit

---
+++ playground/models.py
@@ -0,0 +1,16 @@
+from flask_sqlalchemy import SQLAlchemy
+sb = SQLAlchemy()
+
+class Course(db.Model):
+    __tablename__ = "course"
+    id = db.Column(db.Integer, primary_key = True)
+    course_number = db.Column(db.String, nullable)
+    course_title = db.Column(db.String, nullable)
+
+class RegisteredStudent():
+
+    __tablename__ = "registeredstudents"
+    id = db.Column(db.Integer, primary_key = True)
+    first_name = db.Column(db.String, nullable = False)
+    last_name = db.Column(db.String, nullable = False)
+    grade = db.Column(db.Integer, nullable = False)
